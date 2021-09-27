f6123cb92619c0216a25f5b2c2c72c3dec4fdfe3
benjaminjsteenhoek@gmail.com <benjaminjsteenhoek@gmail.com>
2021-09-27 11:26:16 -0500

    Update get_customers.sql to be correct (I think)

--- get_customers.sql
+++ get_customers.sql
@@ -1,6 +1,6 @@
 -- Get geographic information of all active field boundaries for real customers
 -- Pull bounding boxes if present, otherwise pull full boundary
-select top 1000 fb.FieldId, CASE WHEN fdb.BoundingRegion IS NULL THEN 0 ELSE 1 END as IsBoundingBox, ISNULL(fdb.BoundingRegion, fb.BoundingPolygon).STAsText() as PolygonWKT from FieldBoundaries fb
+select fb.FieldId as FieldId, ISNULL(fdb.BoundingRegion, fb.BoundingPolygon).STAsText() as PolygonWKT from FieldBoundaries fb
 LEFT JOIN FieldDataBounds fdb ON (fb.FieldId = fdb.FieldId)
 INNER JOIN [dbo].[GetValidAccountIds](1) a ON (fb.[AccountId] = a.[AccountId])
 where fb.IsActiveFieldBoundary = 1
